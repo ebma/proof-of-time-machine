fc2071dce78d6be929b9fd436117ab63bf532a1e
Marcel Ebert <mail@marcel-ebert.de>
2020-06-12 15:26:55 +0200

    Use new contracts in drizzleOptions

--- app/src/drizzleOptions.js
+++ app/src/drizzleOptions.js
@@ -1,17 +1,16 @@
 import Web3 from "web3";
-import ComplexStorage from "./contracts/ComplexStorage.json";
-import SimpleStorage from "./contracts/SimpleStorage.json";
-import TutorialToken from "./contracts/TutorialToken.json";
+import IdentityService from "./contracts/IdentityService.json";
+import TimestampFactory from "./contracts/TimestampFactory.json";
 
 const options = {
   web3: {
     block: false,
-    customProvider: new Web3("ws://localhost:7545")
+    customProvider: new Web3("ws://localhost:7545"),
   },
-  contracts: [SimpleStorage, ComplexStorage, TutorialToken],
+  contracts: [IdentityService, TimestampFactory],
   events: {
-    SimpleStorage: ["StorageSet"]
-  }
+    TimestampFactory: ["NewTimestamp"],
+  },
 };
 
 export default options;
