d3735f792e80daf1d1a6129b94a48f219626c9e4
Marcel Ebert <mail@marcel-ebert.de>
2020-06-12 15:12:57 +0200

    Add new contracts to deploy_contracts migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,7 @@
-const SimpleStorage = artifacts.require("SimpleStorage");
-const TutorialToken = artifacts.require("TutorialToken");
-const ComplexStorage = artifacts.require("ComplexStorage");
+const Timestamp = artifacts.require("Timestamp");
+const IdentityService = artifacts.require("IdentityService");
 
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
-  deployer.deploy(TutorialToken);
-  deployer.deploy(ComplexStorage);
+module.exports = function (deployer) {
+  deployer.deploy(Timestamp);
+  deployer.deploy(IdentityService);
 };
