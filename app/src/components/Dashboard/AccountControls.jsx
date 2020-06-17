4377ccfde271a35b1715af0fcae072c1039cc93d
Marcel Ebert <mail@marcel-ebert.de>
2020-06-17 16:05:16 +0200

    Remove unused styles

--- app/src/components/Dashboard/AccountControls.jsx
+++ app/src/components/Dashboard/AccountControls.jsx
@@ -1,33 +1,9 @@
-import { drizzleReactHooks } from "@drizzle/react-plugin";
 import { Box, Typography } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
 import { AppContext } from "../../contexts/app";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-  },
-  box: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: theme.spacing(2),
-  },
-  button: {
-    padding: theme.spacing(1),
-    margin: theme.spacing(2),
-  },
-}));
-
 function AccountControls() {
-  const classes = useStyles();
-
-  const { accounts, currentAccount } = React.useContext(AppContext);
-
-  const { drizzle } = drizzleReactHooks.useDrizzle();
-  const { web3 } = drizzle;
+  const { currentAccount } = React.useContext(AppContext);
 
   const MetamaskInfo = React.useMemo(() => {
     return typeof window.ethereum === "undefined" ? (
