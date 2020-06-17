7921b393f911f301db120bf9e1b98c253982e159
Marcel Ebert <mail@marcel-ebert.de>
2020-06-17 10:14:52 +0200

    Remove customProvider from dizzleOptions This way drizzle will use the window.web3 provider by default which is injected by metamask

--- app/src/drizzleOptions.js
+++ app/src/drizzleOptions.js
@@ -1,12 +1,12 @@
-import Web3 from "web3";
 import IdentityService from "./contracts/IdentityService.json";
 import TimestampFactory from "./contracts/TimestampFactory.json";
 
 const options = {
   web3: {
-    block: false,
-    customProvider: new Web3("ws://localhost:7545"),
-    // customProvider: new Web3("wss://kovan.infura.io/ws/v3/9fe5eb4f33964a93a62ac085ca877f93"),
+    fallback: {
+      type: "ws",
+      url: "ws://localhost:7545",
+    },
   },
   contracts: [IdentityService, TimestampFactory],
   events: {
