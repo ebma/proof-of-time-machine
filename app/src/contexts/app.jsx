265ff5984f01be8c98d19afdf7cc584cd705cafa
Marcel Ebert <mail@marcel-ebert.de>
2020-06-24 14:35:25 +0200

    Add ipfsClient to context

--- app/src/contexts/app.jsx
+++ app/src/contexts/app.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import ipfsClient from "ipfs-http-client";
 
 const AppContext = React.createContext({
   accounts: [],
   currentAccount: undefined,
-  currentBalance: undefined,
+  ipfsClient: undefined,
 });
 
 function AppProvider(props) {
   const [accounts, setAccounts] = React.useState([]);
   const [currentAccount, setCurrentAccount] = React.useState(undefined);
+  const [ipfs, setIpfs] = React.useState(undefined);
 
   React.useEffect(() => {
     if (typeof window.ethereum !== "undefined") {
@@ -22,11 +24,20 @@ function AppProvider(props) {
         setCurrentAccount(accounts[0]);
       });
     }
+
+    const ipfs = ipfsClient({
+      host: "ipfs.infura.io",
+      port: 5001,
+      protocol: "https",
+    });
+
+    setIpfs(ipfs);
   }, []);
 
   const contextValue = {
     accounts,
     currentAccount,
+    ipfsClient: ipfs,
   };
 
   return (
